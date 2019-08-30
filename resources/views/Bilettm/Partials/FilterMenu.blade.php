be241c2883f978e1385a252b0594a30eaed5f6f5
merdiano <merdan.m@gmail.com>
2019-08-30 16:12:35 +0500

    filter menu

---
+++ resources/views/Bilettm/Partials/FilterMenu.blade.php
@@ -0,0 +1,27 @@
+<!-- Nav tabs -->
+<ul class="nav u-nav-v1-1 g-mb-20" role="tablist" data-target="nav-1-1-default-hor-left"
+    data-tabs-mobile-type="slide-up-down" data-btn-classes="btn btn-md btn-block rounded-0 u-btn-outline-lightgray g-mb-20">
+    <li class="nav-item">
+        <a class="nav-link active" data-toggle="tab" href="" role="tab">Популярное</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="" role="tab">Новые</a>
+    </li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" role="tab">Сегодня <i class="fa fa-caret-down"></i></a>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item nav-link" data-toggle="tab" href="" role="tab">SLink 1</a>
+            <a class="dropdown-item nav-link" data-toggle="tab" href="" role="tab">SLink 2</a>
+            <a class="dropdown-item nav-link" data-toggle="tab" href="" role="tab">SLink 3</a>
+        </div>
+    </li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" role="tab">Дата <i class="fa fa-caret-down"></i></a>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item nav-link" data-toggle="tab" href="" role="tab">DLink 1</a>
+            <a class="dropdown-item nav-link" data-toggle="tab" href="" role="tab">DLink 2</a>
+            <a class="dropdown-item nav-link" data-toggle="tab" href="" role="tab">DLink 3</a>
+        </div>
+    </li>
+</ul>
+<!-- End Nav tabs -->
--- resources/views/Bilettm/Partials/HomeCinema.blade.php
+++ resources/views/Bilettm/Partials/HomeCinema.blade.php
@@ -5,38 +5,13 @@
         <a class="" href="">Посмотреть все</a>
     </div>
     <div class="tab-ozi col-12">
-        <!-- Nav tabs -->
-        <ul class="nav u-nav-v1-1 g-mb-20" role="tablist" data-target="nav-1-1-default-hor-left" data-tabs-mobile-type="slide-up-down" data-btn-classes="btn btn-md btn-block rounded-0 u-btn-outline-lightgray g-mb-20">
-            <li class="nav-item">
-                <a class="nav-link active" data-toggle="tab" href="" role="tab">Популярное</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="" role="tab">Новые</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" role="tab">Сегодня <i class="fa fa-caret-down"></i></a>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item nav-link" data-toggle="tab" href="" role="tab">SLink 1</a>
-                    <a class="dropdown-item nav-link" data-toggle="tab" href="" role="tab">SLink 2</a>
-                    <a class="dropdown-item nav-link" data-toggle="tab" href="" role="tab">SLink 3</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" role="tab">Дата <i class="fa fa-caret-down"></i></a>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item nav-link" data-toggle="tab" href="" role="tab">DLink 1</a>
-                    <a class="dropdown-item nav-link" data-toggle="tab" href="" role="tab">DLink 2</a>
-                    <a class="dropdown-item nav-link" data-toggle="tab" href="" role="tab">DLink 3</a>
-                </div>
-            </li>
-        </ul>
-        <!-- End Nav tabs -->
+        @include('Bilettm.Partials.FilterMenu')
 
         <div class="owl-carousel container row" id="kinoteator-tab1" style="padding: 0 !important; margin: 0">
             <div class="slider-slider">
                 <div class="row">
                     <div class="col-6">
-                        @include('Bilettm.Partials.CinemaItem',['event'=>$cinema->shift(1)])
+                        @include('Bilettm.Partials.CinemaItem',['event'=>$cinema->shift(1),'size'=>'big'])
                     </div>
                     <div class="col-6">
                         <div class="row">
