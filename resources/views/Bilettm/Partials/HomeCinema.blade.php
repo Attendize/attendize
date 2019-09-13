dfa8afb10f9c790d6b1b3f40e5baeeaff893c083
Medet <medet-kadyr2020@mail.ru>
2019-09-13 12:28:14 +0500

    first front

--- resources/views/Bilettm/Partials/HomeCinema.blade.php
+++ resources/views/Bilettm/Partials/HomeCinema.blade.php
@@ -1,14 +1,14 @@
 <section id="kinoteator" class="kinoteator-section container">
-    <div class="tab-header d-flex justify-content-between col-12">
+    <div class="tab-header d-flex justify-content-between col-12 px-0">
         <h2 class="">Кинотеатры</h2>
-        <div style="height: 5px; margin-left: 5px; position: absolute; bottom: 10px; width: 100px; background-color: rgba(211,61,51,1)"></div>
+        <div style="height: 5px; position: absolute; bottom: 0; width: 100px; background-color: rgba(211,61,51,1)"></div>
         <a class="" href="">Посмотреть все</a>
     </div>
-    <div class="tab-ozi col-12">
+    <div class="tab-ozi col-12 px-0">
 
         <div class="owl-carousel container row" id="kinoteator-tab1" style="padding: 0 !important; margin: 0">
             <div class="slider-slider">
-                <div class="row">
+                <div class="row w-100 m-auto">
                     <div class="col-6">
                         @include('Bilettm.Partials.CinemaItem',['event'=>$cinema->shift(1),'size'=>'big'])
                     </div>
