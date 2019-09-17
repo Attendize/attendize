b41268ada5b29e476fd0b284d009bfbd05f0bcbf
merdiano <merdan.m@gmail.com>
2019-09-17 14:19:35 +0500

    map

--- resources/views/Bilettm/ViewEvent/EventPage.blade.php
+++ resources/views/Bilettm/ViewEvent/EventPage.blade.php
@@ -23,6 +23,16 @@
                                         <b property="name">{{$event->venue_name}}</b>
                                         <meta property="address" content="{{ urldecode($event->venue_name) }}">
                                     </span>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="google-maps content">
+                                                <iframe frameborder="0" style="border:0;"
+                                                        src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q={{$event->map_address}}&amp;aq=0&amp;oq={{$event->map_address}}&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq={{$event->map_address}}&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed">
+
+                                                </iframe>
+                                            </div>
+                                        </div>
+                                    </div>
                             </div>
                             @include('Bilettm.ViewEvent.Partials.TicketSchedule')
                         </div>
@@ -32,6 +42,7 @@
 
                     @include('Bilettm.ViewEvent.Partials.EventShareButtons')
                     <img src="{{asset('assets/images/advs/adv.png')}}" style="width: 100%">
+
                 </div>
                 {{--<div class="col-12 p-0">--}}
                     {{--@include('Bilettm.Partials.EventTags')--}}
@@ -40,16 +51,7 @@
         </div>
     </section>
     <section id="location" class="container p0">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="google-maps content">
-                    <iframe frameborder="0" style="border:0;"
-                            src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q={{$event->map_address}}&amp;aq=0&amp;oq={{$event->map_address}}&amp;sll=28.659344,-81.187888&amp;sspn=0.128789,0.264187&amp;ie=UTF8&amp;hq={{$event->map_address}}&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed">
 
-                    </iframe>
-                </div>
-            </div>
-        </div>
     </section>
 @endsection
 @section('after_scripts')
